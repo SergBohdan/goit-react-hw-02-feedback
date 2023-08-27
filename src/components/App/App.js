7d88bfc44f2b6b005de91918d7bb53eec0cad6a0
Serhii Bogdanov <bohdanov.serhey@gmail.com>
2023-08-27 11:27:53 +0300

    Update App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,31 +1,69 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { GlobalStyle } from '../GlobalStyles';
-
 
 export class App extends Component {
   state = {
     good: 0,
     neutral: 0,
-    bad: 0
+    bad: 0,
   };
 
-  handleIncrement = () =>{
-    console.log('click increase value');
-    console.log(this);
+  countPositiveFeedbackPercentage = () => {
+    const { good } = this.state;
+    const totalFeedback = this.countTotalFeedback();
+
+    return totalFeedback ? ((good * 100) / totalFeedback).toFixed(1) : 0;
   };
 
-render(){
-  return(
-<div>
-<span>0</span>
- <div>
-  <button type='button' onClick={this.handleIncrement}>increase 1</button>
-  <button type='button'>decrease 1</button>
- </div>
-<GlobalStyle />
-</div>
-  );
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+
+  handleIncrement = type => {
+    this.setState(prevState => ({
+      ...prevState,
+      [type]: prevState[type] + 1,
+    }));
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    return (
+      <div>
+        <h2>Please leave feedback</h2>
+        <button type="button" onClick={() => this.handleIncrement('good')}>
+          good
+        </button>
+        <button type="button" onClick={() => this.handleIncrement('neutral')}>
+          neutral
+        </button>
+        <button type="button" onClick={() => this.handleIncrement('bad')}>
+          bad
+        </button>
+
+        <div>
+          <h2>Statistics</h2>
+          <p>
+            Good: <span>{good}</span>
+          </p>
+
+          <p>
+            Neutral: <span>{neutral}</span>
+          </p>
+          <p>
+            Bad: <span>{bad}</span>
+          </p>
+
+          <p>
+            Total: <span>{this.countTotalFeedback('total')}</span>
+          </p>
+          <p>Positive feedback: <span>{this.countPositiveFeedbackPercentage('percent')}</span></p>
+        </div>
+
+        <GlobalStyle />
+      </div>
+    );
+  }
 }
-};
-
 export default App;
